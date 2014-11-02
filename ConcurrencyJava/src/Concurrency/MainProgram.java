9a1d342cf222fa1379c7b4711528c0ea514a35f3
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2014-11-02 17:53:02 -0600

    Updated ConcurrencyTest

--- ConcurrencyJava/src/Concurrency/MainProgram.java
+++ ConcurrencyJava/src/Concurrency/MainProgram.java
@@ -1,16 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Concurrency;
 
+import java.io.IOException;
+import java.util.concurrent.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author Jay C Espinoza
  */
 public class MainProgram {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("\"*** Ejemplo de Sincronizacion de 2 procesos *****\"");
+        
+        ProcessI P0, P1;
+        P0 = new ProcessI(0);
+        P1 = new ProcessI(1);
+        
+        Thread t1, t2;
+        t1 = new Thread(P0);
+        t2 = new Thread(P1);
+        
+        t1.start();
+        t2.start();
+
+        System.out.println("Press any key to continue . . .");
+        try {
+            System.in.read();
+        } catch (IOException ex) {
+            Logger.getLogger(MainProgram.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }

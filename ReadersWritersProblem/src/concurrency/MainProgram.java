eb88c895ed94bd5339d24b123ce10775fda63839
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2014-11-02 21:46:44 -0600

    Added code to test the processes

--- ReadersWritersProblem/src/concurrency/MainProgram.java
+++ ReadersWritersProblem/src/concurrency/MainProgram.java
@@ -1,5 +1,7 @@
 package concurrency;
 
+import java.util.concurrent.Semaphore;
+
 /**
  * @author Jay C Espinoza
  */
@@ -9,8 +11,32 @@ public class MainProgram {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Testing the Application");
+        Semaphore writerSem = new Semaphore(1); //To prevent more than 1 writer
+        Semaphore readerMutExSem = new Semaphore(1); //To prevent mutual exclusion between readers
         
+        /***Readers***/
+        ReaderProcess RP0 = new ReaderProcess(writerSem, readerMutExSem);
+        ReaderProcess RP1 = new ReaderProcess(writerSem, readerMutExSem);
+        ReaderProcess RP2 = new ReaderProcess(writerSem, readerMutExSem);
         
+        /*** Writers ***/
+        WriterProcess WP0 = new WriterProcess(writerSem);
+        WriterProcess WP1 = new WriterProcess(writerSem);
+        
+        /*** Threads ***/
+        Thread tr0 = new Thread(RP0);
+        Thread tr1 = new Thread(RP1);
+        Thread tr2 = new Thread(RP2);
+        
+        Thread tw0 = new Thread(WP0);
+        Thread tw1 = new Thread(WP1);
+        
+        tr1.start();
+        tw1.start();
+        tr0.start();
+        tw0.start();
+        tr2.start();
+        
+        System.out.println("Main Process Finished Execution");
     }
 }
